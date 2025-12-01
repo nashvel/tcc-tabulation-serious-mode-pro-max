47fad0f3dbb7e1fb5aa3c0319d1c3b6d9a60d7a7
nacht <nash.backup21@gmail.com>
2025-12-02 07:08:15 +0800

    removed electron

--- desktop/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-  startService: (serviceType) => ipcRenderer.invoke('start-service', serviceType),
-  stopService: (serviceType) => ipcRenderer.invoke('stop-service', serviceType),
-  checkServiceStatus: (serviceType) => ipcRenderer.invoke('check-service-status', serviceType),
-  openBrowser: (url) => ipcRenderer.invoke('open-browser', url)
-});
